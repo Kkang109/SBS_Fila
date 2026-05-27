d8a9c9a37f6377f2f2457140193e9808b8bec5e6
Kkang109 <twilight03@kakao.com>
2026-05-27 11:26:35 +0900

    끝

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 // 트렌딩 목록 탭
-
 function init(){
 	for(let j = 0; j < list.length; j++){
 		list[j].classList.remove('active');
@@ -20,4 +19,64 @@ for(let i = 0; i < list.length; i++) {
 	}
 }
 
-//
+// 팝업 레이어
+let look = document.querySelectorAll('.slide_container li');
+let popup = document.querySelector('#popup');
+let close = document.querySelector('.btn-close');
+let popbg = document.querySelector('#popup .bg');
+
+for(let i = 0; i < look.length; i++) {
+	look[i].onclick = () => {
+		popup.style.display = 'block';
+	}
+}
+
+close.onclick = () => {
+	popup.style.display = 'none';
+}
+
+popbg.onclick = () => {
+	popup.style.display = 'none';
+}
+
+// 인포 1 섹션 h2 애니메이션
+window.addEventListener('scroll', function() {
+	
+	const h2Element = document.querySelector('.information1_section > h2');
+	const elementPosition = h2Element.getBoundingClientRect().top;
+	const screenPosition = window.innerHeight;
+	
+	if (elementPosition < screenPosition) {
+		h2Element.classList.add('active');
+	}
+});
+
+
+// 히어로 섹션 캐러셀
+
+const carousel = document.querySelector('.carousel');
+const totalSlides = carousel.children.length;
+
+let move = 0;
+
+// 1. 오른쪽 버튼을 눌렀을 때 (다음 이미기 보기)
+document.querySelector('.right_btn').onclick = () => {
+
+	if (move === -(totalSlides - 1) * 100) {
+		move = 0;
+	} else {
+		move = move - 100;
+	}
+	carousel.style.transform = `translateX(${move}%)`;
+};
+
+// 2. 왼쪽 버튼을 눌렀을 때 (이전 이미지 보기)
+document.querySelector('.left_btn').onclick = () => {
+
+	if (move === 0) {
+		move = -(totalSlides - 1) * 100;
+	} else {
+		move = move + 100;
+	}
+	carousel.style.transform = `translateX(${move}%)`;
+};
